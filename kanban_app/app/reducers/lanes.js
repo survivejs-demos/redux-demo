6315b4ddacf6c93cde7fdfa165996297ba60d175
Juho Vepsalainen <bebraw@gmail.com>
2015-09-03 09:15:23 +0300

    Tidy up imports

--- kanban_app/app/reducers/lanes.js
+++ kanban_app/app/reducers/lanes.js
@@ -1,29 +1,25 @@
-import {
-  CREATE_LANE, UPDATE_LANE, DELETE_LANE,
-  ATTACH_TO_LANE, DETACH_FROM_LANE,
-  MOVE_FROM_LANE_TO_LANE
-} from '../constants/LaneActionTypes';
+import * as types from '../constants/LaneActionTypes';
 
 const initialState = [];
 
 export default function lanes(state = initialState, action) {
   switch (action.type) {
-    case CREATE_LANE:
+    case types.CREATE_LANE:
       return [];
 
-    case UPDATE_LANE:
+    case types.UPDATE_LANE:
       return [];
 
-    case DELETE_LANE:
+    case types.DELETE_LANE:
       return [];
 
-    case ATTACH_TO_LANE:
+    case types.ATTACH_TO_LANE:
       return [];
 
-    case DETACH_FROM_LANE:
+    case types.DETACH_FROM_LANE:
       return [];
 
-    case MOVE_FROM_LANE_TO_LANE:
+    case types.MOVE_FROM_LANE_TO_LANE:
       return [];
 
     default:
