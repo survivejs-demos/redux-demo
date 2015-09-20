b39af9dc850e2842cf12f38137f3615d30e20c78
Juho Vepsalainen <bebraw@gmail.com>
2015-09-20 19:58:49 +0300

    Push Root to a file of its own
    Neater this way.

--- multi_app/app/index.jsx
+++ multi_app/app/index.jsx
@@ -7,38 +7,15 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd/modules/backends/HTML5';
-import { Provider } from 'react-redux';
-import { ReduxRouter } from 'redux-router';
-import { Route, Link } from 'react-router';
+
 import configureStore from './store/configureStore';
 import storage from './libs/storage';
-
-import Home from './views/Home.jsx';
-import Board from './views/Board.jsx';
+import createRoot from './Root.jsx';
 
 const APP_STORAGE = 'app';
 
 const store = configureStore(storage.get(APP_STORAGE) || {});
-
-@DragDropContext(HTML5Backend)
-class Root extends React.Component {
-  // XXX: render Home, figure out why routing resets to root
-  render() {
-    return (
-      <div>
-        <Provider store={store}>
-          <ReduxRouter>
-            <Route path="/" component={Board}>
-              <Route path=":name" component={Board} />
-            </Route>
-          </ReduxRouter>
-        </Provider>
-      </div>
-    );
-  }
-}
+const Root = createRoot(store);
 
 main();
 
