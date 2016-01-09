7f7c43b837c70cdfc35126087e0c09bdad351924
Juho Vepsalainen <bebraw@gmail.com>
2016-01-09 14:40:55 +0200

    Add action logging

--- kanban_app/app/store/configureStore.js
+++ kanban_app/app/store/configureStore.js
@@ -1,13 +1,25 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
+const logger = createLogger(
+  {
+    collapsed: true,
+    predicate: (getState, action) => process.env.NODE_ENV !== 'production',
+  }
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  logger
+)(createStore);
+
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStoreWithMiddleware(rootReducer, initialState);
 
   if(module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers');
+      const nextReducer = require('../reducers/index');
 
       store.replaceReducer(nextReducer);
     });
